29de88f365d00d8dc37191e3e73c768a953f608e
vaithi227 <30289514+vaithi227@users.noreply.github.com>
2017-08-31 15:20:28 +0530

    Update environment.ts

--- spring-petclinic-angular/src/environments/environment.ts
+++ spring-petclinic-angular/src/environments/environment.ts
@@ -23,5 +23,5 @@
 //  REST_API_URL: 'http://localhost:9966/petclinic/api/'
 export const environment = {
   production: false,
-   REST_API_URL: 'http://localhost:9966/petclinic/api/'
+   REST_API_URL: 'http://localhost:9966/api/'
 };
